615c62158049a22b78e84a36ac7bb7f0aaaad552
Ripal Patel <ripal212007@gmail.com>
2018-11-07 20:09:34 -0600

    1 part is displaying

--- src/components/LegoCharacters/legoCharacter.js
+++ src/components/LegoCharacters/legoCharacter.js
@@ -1,2 +1,36 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
+import printParts from '../../data/partsData';
+
+const creatPartsList = (part, divID) => {
+  let domString = '';
+  domString += `<div class="head-card">
+                    <img src=${part.imageUrl} width="250px">
+                    <h4>${part.name}</h4>`;
+  $(divID).html(domString);
+};
+
+const initializeHeadView = () => {
+  printParts.getHeads().then((data) => {
+    creatPartsList(data, '#lego-head');
+  }).catch((error) => {
+    console.error(error);
+  });
+};
+
+const initializeTorsoView = () => {
+  printParts.getTorsos().then((data) => {
+    creatPartsList(data, '#lego-torso');
+  }).catch((error) => {
+    console.error(error);
+  });
+};
+
+const initializeLegsView = () => {
+  printParts.getLegs().then((data) => {
+    creatPartsList(data, '#lego-legs');
+  }).catch((error) => {
+    console.error(error);
+  });
+};
+export default { initializeHeadView, initializeTorsoView, initializeLegsView };
--- src/data/partsData.js
+++ src/data/partsData.js
@@ -1,2 +1,36 @@
-// import $ from 'jquery';
+
+import axios from 'axios';
 import 'bootstrap';
+
+// const getAllHeadsFromDb = axios.get('http://localhost:3001/heads');
+
+const getHeads = () => new Promise((resolve, reject) => {
+  axios.get('http://localhost:3001/heads')
+    .then((data) => {
+      const allHeads = data.data[0];
+      resolve(allHeads);
+    }).catch((error) => {
+      reject(error);
+    });
+});
+
+const getTorsos = () => new Promise((resolve, reject) => {
+  axios.get('http://localhost:3001/torsos')
+    .then((data) => {
+      const allTorsos = data.data[0];
+      resolve(allTorsos);
+    }).catch((error) => {
+      reject(error);
+    });
+});
+
+const getLegs = () => new Promise((resolve, reject) => {
+  axios.get('http://localhost:3001/legs')
+    .then((data) => {
+      const allLegs = data.data[0];
+      resolve(allLegs);
+    }).catch((error) => {
+      reject(error);
+    });
+});
+export default { getHeads, getTorsos, getLegs };
